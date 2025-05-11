8dbd5abe0e157d5c1c36a99bcb46610d267af5ce
Your Name <you@example.com>
2025-05-11 19:17:19 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,32 +3,38 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {    // 1. method main must be defined
 
-        for (int i = 1; i < 100; i++) {
+        int i = 1;
+        while (i <= 100) {
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+            i = playFizzBuzz(i);
         }
+    }
+
+    private static int playFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0; // 2. uses ; to end statements
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) { // 3. open and ends a for or if statement by {}
+
+            System.out.println("Fizz Buzz"); // 4. print statement is different from python
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+        i++;
+        return i;
     }
 }
