e331b8e3cb6773f42cb942fda672ba7d3f0f8aaa
Your Name <you@example.com>
2025-05-12 10:57:25 -0400

    method for Reduce made

---
+++ src/Reduce.java
@@ -0,0 +1,17 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int step = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+                step++;
+            }
+            if (n % 2 != 0) {
+                n = n - 1;
+                step++;
+            }
+        }
+        System.out.println(step);
+    }
+}
